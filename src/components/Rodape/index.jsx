1092090aeb069ae24da9701e14873ada8117554b
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2023-07-06 16:44:43 -0300

    update rodape

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -3,7 +3,77 @@ import './Rodape.css'
 const Rodape = () => {
     return (
         <>
-            <footer>
+            <footer className='Rodape'>
+                <div>
+                    <h3>Veículos</h3>
+                    <ul>
+                        <li>Todos</li>
+                        <li>Carros</li>
+                        <li>SUVS e Crossovers</li>
+                        <li>Picapes</li>
+                        <li>Esportivos</li>
+                        <li>Eléttricos</li>
+                        <li>Futuro EV</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Comprar</h3>
+                    <ul>
+                        <li>Consulte Nosso Estoque</li>
+                        <li>Ofertas</li>
+                        <li>Solicitar contato</li>
+                        <li>Encontre Uma Concessionária </li>
+                        <li>Vendas Diretas</li>
+                        <li>Financie seu Chevrolet</li>
+                        <li>Chevrolet Sempre</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SERVIÇOS</h3>
+                    <ul>
+                        <li>Serviços Chevrolet</li>
+                        <li>Agendar Serviço</li>
+                        <li>OnStar</li>
+                        <li>Acessórios</li>
+                        <li>Revisão</li>
+                        <li>Road Service</li>
+                        <li>MyLink</li>
+                        <li>Recalls</li>
+                        <li>Recall Airbag Takata</li>
+                        <li>Últimos Recalls</li>
+                        <li>Manuais De Proprietários</li>
+                        <li>Proconve</li>
+                        <li>Manuais de Resgate</li>
+                        <li>Programa de Rotulagem Veicular</li>
+                        <li>Tutoriais</li>
+                        <li>Chevrolet AcessiLab</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SOBRE A CHEVROLET</h3>
+                    <ul>
+                        <li>Prêmios</li>
+                        <li>Instituto GM</li>
+                        <li>Trabalhe Conosco</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>AJUDA</h3>
+                    <ul>
+                        <li>Fale Conosco</li>
+                        <li>Política De Privacidade</li>
+                        <li>Avisos Importantes</li>
+                        <li>Portal de Privacidade de Dados</li>
+                        <li>Mapa do Site</li>
+                    </ul>
+                    <h3>NOSSAS REDES</h3>
+                    <a href="#"><img src="" /></a>
+                    <a href="#"><img src="" /></a>
+                    <a href="#"><img src="" /></a>
+                    <a href="#"><img src="" /></a>
+                    <a href="#"><img src="" /></a>
+                </div>
+
                 
             </footer>
         </>
